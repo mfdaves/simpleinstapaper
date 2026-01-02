056085054dc175350f798b6bf27f6041f4514116
mfdaves <mfdaves@proton.me>
2026-01-03 00:14:20 +0100

    first

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use reqwest::Client;
+use std::env;
+
+pub static BASE_URL: &str = "https://www.instapaper.com/api";
+
+pub struct InstapaperSimpleClient {
+    pub client: Client,
+    pub username: String,
+    pub password: String,
+}
+
+impl InstapaperSimpleClient {
+    pub fn new() -> Self {
+        Self {
+            client: Client::new(),
+            username: env::var("INSTAPAPER_USERNAME")
+                .expect("Username is mandatory. Check your env var"),
+            password: env::var("INSTAPAPER_PASSWORD")
+                .expect("Password is mandatory. Check your env var."),
+        }
+    }
+
+    pub fn with_credentials(username: String, password: String) -> Self {
+        Self {
+            client: Client::new(),
+            username,
+            password,
+        }
+    }
+
+    pub async fn auth(&self) -> Result<bool, reqwest::Error> {
+        Ok(self
+            .client
+            .get(format!("{BASE_URL}/authenticate"))
+            .query(&[
+                ("username", self.username.as_str()),
+                ("password", self.password.as_str()),
+            ])
+            .send()
+            .await?
+            .status()
+            .is_success())
+    }
+
+    pub async fn add<I: Into<String>>(&self, url: I) -> Result<bool, reqwest::Error> {
+        Ok(self
+            .client
+            .get(format!("{BASE_URL}/add"))
+            .query(&[
+                ("username", self.username.as_str()),
+                ("password", self.password.as_str()),
+                ("url", url.into().as_str()),
+            ])
+            .send()
+            .await?
+            .status()
+            .is_success())
+    }
+}
